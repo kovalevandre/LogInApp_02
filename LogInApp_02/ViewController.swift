618e8087348bafea3256e371d7345dbf17325fcb
Andrey <akovalev@me.com>
2023-10-08 11:04:55 +0300

    Initial Commit

---
+++ LogInApp_02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LogInApp_02
+//
+//  Created by Andrey Kovalev on 08.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
