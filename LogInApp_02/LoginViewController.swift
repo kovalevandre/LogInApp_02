7b3f5bf72c21ba709187c3c5daeb115f554feb9e
Andrey <akovalev@me.com>
2023-10-08 17:11:12 +0300

    Last update

--- LogInApp_02/LoginViewController.swift
+++ LogInApp_02/LoginViewController.swift
@@ -9,21 +9,56 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func forgotUserNameButtonTapped(_ sender: UIButton) {
+        showAlert(title: "Forgot User Name?", message: "User name is 'User_23'")
+    }
+
+    @IBAction func forgotPasswordButtonTapped(_ sender: UIButton) {
+        showAlert(title: "Forgot Password?", message: "Password is 'gateway'")
+    }
+
+    private func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        userNameTextField.autocorrectionType = .no
+        passwordTextField.autocorrectionType = .no
+        passwordTextField.isSecureTextEntry = true
+        
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        guard let enteredUserName = userNameTextField.text, let enteredPassword = passwordTextField.text else {
+            return false
+        }
+        
+        if enteredUserName == "User_23" && enteredPassword == "gateway" {
+            return true
+        } else {
+            showAlert(title: "Error", message: "Wrong User Name or Password")
+            return false
+        }
+    }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if let welcomeVC = segue.destination as? WelcomeViewController {
+            welcomeVC.userName = userNameTextField.text ?? ""
+        }
     }
-    */
 
+    @IBAction func unwindToLogin(_ unwindSegue: UIStoryboardSegue) {
+                userNameTextField.text = ""
+                passwordTextField.text = ""
+        
+    }
 }
--- LogInApp_02/WelcomeViewController.swift
+++ LogInApp_02/WelcomeViewController.swift
@@ -9,21 +9,20 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
+    @IBOutlet weak var welcomeLabel: UILabel!
+    var userName: String?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        if let userName = userName {
+            welcomeLabel.text = "Welcome, \(userName)!"
+        }
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func logoutButtonTapped(_ sender: UIButton) {
+        userName = nil
+        welcomeLabel.text = ""
+        
     }
-    */
-
 }
